c5e008c3410849cb691a9f50c3f2ffc67d80cce9
Muhammad Hasyim <mh7373@log-3.hpc.nyu.edu>
2025-06-19 19:16:39 -0400

    Delete old build dir

--- build/src/version.py
+++
@@ -1,6 +0,0 @@
-# Copyright (c) 2009-2025 The Regents of the University of Michigan.
-# Part of HOOMD-blue, released under the BSD 3-Clause License.
-
-"""Version information."""
-
-version = '0.0.0'
